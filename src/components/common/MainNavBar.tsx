7df18b878534ddcbaa8972a2d5489edf15dc4049
Anthony Lee <anthonylee1994@users.noreply.github.com>
2025-06-27 12:07:01 +0800

    refactor: Move NavbarMenuToggle into a separate NavbarContent for improved layout and responsiveness in MainNavBar

--- src/components/common/MainNavBar.tsx
+++ src/components/common/MainNavBar.tsx
@@ -32,8 +32,10 @@ export const MainNavBar = () => {
             onMenuOpenChange={setIsMenuOpen}
             maxWidth="full"
         >
+            <NavbarContent className="md:hidden">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+            </NavbarContent>
             <NavbarContent>
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="md:hidden" />
                 <NavbarBrand>
                     <Icon icon="fa-solid:wallet" className="mr-2 select-none text-2xl text-primary-500 dark:text-white" />
                     <span className="select-none bg-gradient-to-r from-primary-500 to-secondary-500 bg-clip-text text-2xl font-bold text-transparent dark:from-white dark:to-gray-300">BudgetHero</span>
